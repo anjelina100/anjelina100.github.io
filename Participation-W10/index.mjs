350fb9bc80bc4d7e39411ba8ea057cf1e6bae0fa
Anjelina Jasso <115503640+anjelina100@users.noreply.github.com>
2026-03-26 12:25:15 -0700

    Add files via upload

---
+++ Participation-W10/index.mjs
@@ -0,0 +1,68 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+//for Express to get values using the POST method
+app.use(express.urlencoded({extended:true}));
+//setting up database connection pool, replace values in red
+const pool = mysql.createPool({
+    host: "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "w9c7lwn8um1o99yj",
+    password: "u3rw8lbcasz2h307",
+    database: "pyn5h5u7iu857dd2",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+//routes
+app.get('/', async (req, res) => {
+   let sql = `SELECT authorId, firstName, lastName
+              FROM authors
+              ORDER BY lastName`;
+   const [authors] = await pool.query(sql);              
+   res.render('home.ejs', {authors})
+});
+
+
+app.get('/searchByAuthor', async (req, res) => {
+   let authorId = req.query.authorId;
+   let sql = `SELECT authorId, quote, firstName, lastName
+             FROM quotes
+             NATURAL JOIN authors
+             WHERE authorId = ?`;
+   let sqlParams = [authorId];
+   const [rows] = await pool.query(sql, sqlParams);              
+   res.render('quotes.ejs', {rows})
+});
+//Searching quotes by keyword
+//NEVER have user input within the SQL statement!!
+app.get("/searchByKeyword", async(req, res) => {
+   try {
+        //console.log(req);
+        let keyword = req.query.keyword;
+        let sql = `SELECT quote, firstName, lastName
+                   FROM quotes
+                   NATURAL JOIN authors
+                   WHERE quote LIKE ? `;
+        let sqlParams = [`%${keyword}%`];
+        const [rows] = await pool.query(sql, sqlParams);
+        res.render("quotes.ejs", {rows});
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error!");
+    }
+});//dbTest
+
+
+app.get("/dbTest", async(req, res) => {
+   try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error!");
+    }
+});//dbTest
+app.listen(3000, ()=>{
+    console.log("Express server running")
+})
